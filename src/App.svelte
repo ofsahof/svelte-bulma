6f6e7333d543e33284bd692f8bcfbb786dff5bd5
Ömer Faruk Şahin <ofsahof@gmail.com>
2021-06-17 18:37:13 +0300

    feat: added Box component

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,17 @@
 <script lang="ts">
   import Button from "./lib/Button.svelte";
+  import Box from "./lib/Box.svelte";
 </script>
 
-<Button
-  className="mt-5 ml-5"
-  type="link"
-  size="medium"
-  text="Click me"
-  loading
-  disabled
-/>
+<Box> 
+  <h1>Deneme 12314123123</h1>
+</Box>
 
-<!--  OR
 <Button
   className="mt-5 ml-5"
   type="link"
   size="medium"
   loading
   disabled>
-  click me 
+  click me
 </Button>
--->
