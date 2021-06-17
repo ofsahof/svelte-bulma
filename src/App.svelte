828854330ba95941aa7c1c89694284eddc108400
Ömer Faruk Şahin <ofsahof@gmail.com>
2021-06-17 19:16:08 +0300

    feat: added Delete component

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,27 @@
 <script lang="ts">
   import Button from "./lib/Button.svelte";
   import Box from "./lib/Box.svelte";
+  import Delete from "./lib/Delete.svelte";
 </script>
 
-<Box> 
-  <h1>Deneme 12314123123</h1>
-</Box>
+<div class="block">
+  <span class="tag is-success">
+    Hello World
+    <Delete size="small"></Delete>
+  </span>
+</div>
 
-<Button
-  className="mt-5 ml-5"
-  type="link"
-  size="medium"
-  >
-  click me
-</Button>
+<div class="notification is-danger">
+  <Delete></Delete>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit
+</div>
+
+<article class="message is-info">
+  <div class="message-header">
+    Info
+    <Delete></Delete>
+  </div>
+  <div class="message-body">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
+  </div>
+</article>
---
+++ src/lib/Delete.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import type { TSize } from "../types/index";
+    export let size: TSize = "";
+
+    $: deleteSize = () => {
+    return `${size ? `is-${size}` : ""}`;
+  };
+</script>
+
+<button on:click class={`delete ${deleteSize()}`}></button>
