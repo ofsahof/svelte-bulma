6af17059a6c3a659a7b830f2630c47d624999880
Ömer Faruk Şahin <ofsahof@gmail.com>
2021-06-17 22:49:45 +0300

    feat: added Input component

--- src/App.svelte
+++ src/App.svelte
@@ -2,26 +2,5 @@
   import Button from "./lib/Button.svelte";
   import Box from "./lib/Box.svelte";
   import Delete from "./lib/Delete.svelte";
+  import Input from "./lib/Input.svelte";
 </script>
-
-<div class="block">
-  <span class="tag is-success">
-    Hello World
-    <Delete size="small"></Delete>
-  </span>
-</div>
-
-<div class="notification is-danger">
-  <Delete></Delete>
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing elit
-</div>
-
-<article class="message is-info">
-  <div class="message-header">
-    Info
-    <Delete></Delete>
-  </div>
-  <div class="message-body">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque risus mi, tempus quis placerat ut, porta nec nulla. Vestibulum rhoncus ac ex sit amet fringilla. Nullam gravida purus diam, et dictum felis venenatis efficitur. Aenean ac eleifend lacus, in mollis lectus. Donec sodales, arcu et sollicitudin porttitor, tortor urna tempor ligula, id porttitor mi magna a neque. Donec dui urna, vehicula et sem eget, facilisis sodales sem.
-  </div>
-</article>
---
+++ src/lib/Input.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import type { TSize, TAccent } from "../types/index";
+  export let type = "input";
+  export let placeholder = "";
+  export let style: TAccent = "";
+  export let size: TSize = "";
+  export let rounded = false;
+  export let hovered = false;
+  export let focused = false;
+  export let classname = "";
+
+  $: inputType = () => {
+    return `${style ? `is-${style}` : ""}`;
+  };
+
+  $: inputSize = () => {
+    return `${size ? `is-${size}` : ""}`;
+  };
+</script>
+
+<input
+  {type}
+  {placeholder}
+  class:is-rounded={rounded}
+  class:is-focused={focused}
+  class:is-hovered={hovered}
+  on:keydown
+  on:keypress
+  on:keyup
+  class={`${classname} input ${inputType()} ${inputSize()}`}
+/>
