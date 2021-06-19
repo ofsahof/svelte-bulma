c1e9a5ec24882cb3f9ad6c12dd56741e415f558e
Ömer Faruk Şahin <ofsahof@gmail.com>
2021-06-19 22:19:30 +0300

    feat: added Notification component

--- src/App.svelte
+++ src/App.svelte
@@ -4,15 +4,10 @@
   import Delete from "./lib/Delete.svelte";
   import Input from "./lib/Input.svelte";
   import Message from "./lib/Message.svelte";
+  import Notification from "./lib/Notification.svelte";
 </script>
 
-<Message color="danger" size="large">
-  <svelte:fragment slot="messageHeader">
-    <p slot="messageHeader">Warning</p>
-    <Delete slot="messageHeader"></Delete>
-  </svelte:fragment>
-
-  <svelte:fragment slot="messageBody">
-    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga odio necessitatibus ipsa repellat autem repellendus, quam nam deserunt id a sit laborum et quia vero nulla, dolor tempora ducimus quod?
-  </svelte:fragment>
-</Message>
+<Notification type="info">
+  <Delete></Delete>
+  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam accusantium minus voluptatum accusamus, aliquid vitae labore iusto porro totam quos ab perferendis tempora reprehenderit vero velit minima voluptatibus, voluptatem illum.
+</Notification>
---
+++ src/lib/Notification.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import type { TAccent } from "../types/index";
+
+  export let type: TAccent = "";
+  export let light = false;
+
+  $: notificationType = () => {
+    return `${type ? `is-${type}` : ""}`;
+  };
+</script>
+
+<div class:is-light={light} class={`notification ${notificationType()}`}>
+  <slot />
+</div>
